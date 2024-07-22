2a8875241a1547bf9f015a5b479303f45a26d9c0
Комков Константин <volt1003@yandex.ru>
2024-07-22 18:21:39 +0300

    Fixed application init

--- lib/di/injection.dart
+++ lib/di/injection.dart
@@ -1,13 +1,28 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'injection.config.dart';
 
 final getIt = GetIt.instance;
 
-@InjectableInit(  
-  initializerName: 'init', 
-  preferRelativeImports: true, 
-  asExtension: true, 
-)  
+@InjectableInit(
+  initializerName: 'init',
+  preferRelativeImports: true,
+  asExtension: true,
+)
 void configureDependencies() => getIt.init();
+
+@module
+abstract class RegisterModule {
+  @preResolve
+  Future<SharedPreferences> get prefs => SharedPreferences.getInstance();
+
+  @singleton
+  InternetConnectionChecker get internetConnectionChecker;
+
+  @singleton
+  Connectivity get connectivity;
+}
