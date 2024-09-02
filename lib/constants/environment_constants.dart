a297086c1ab32f930e9f14be9bc410a5a6756f8a
Konstantin Komkov <38656572+KonstantenKomkov@users.noreply.github.com>
2024-09-02 21:06:30 +0300

    Fixed baseUrl

--- lib/constants/environment_constants.dart
+++ lib/constants/environment_constants.dart
@@ -2,8 +2,8 @@ import 'package:flutter/services.dart';
 
 class EnvironmentConstants {
   String get baseUrl => appFlavor == 'dev'
-      ? "https://staging.foodriver.site/game"
-      : "https://staging.foodriver.site/game";
+      ? "https://staging.foodriver.site"
+      : "https://staging.foodriver.site";
   String get privacyPolicyUrl => appFlavor == 'dev'
       ? "https://staging.foodriver.site/game/api/v1/files/TermsofServices.pdf"
       : "https://staging.foodriver.site/game/api/v1/files/TermsofServices.pdf";
