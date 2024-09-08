db2437f9691ff499c2077a0392f454f28f8b703a
Konstantin Komkov <38656572+KonstantenKomkov@users.noreply.github.com>
2024-09-08 16:06:46 +0300

    Change privacy policy url

--- lib/constants/environment_constants.dart
+++ lib/constants/environment_constants.dart
@@ -5,6 +5,6 @@ class EnvironmentConstants {
       ? "https://staging.foodriver.site"
       : "https://staging.foodriver.site";
   String get privacyPolicyUrl => appFlavor == 'dev'
-      ? "https://staging.foodriver.site/files/api/v1/file/TermsofServices.pdf"
-      : "https://staging.foodriver.site/files/api/v1/file/TermsofServices.pdf";
+      ? "https://staging.foodriver.site/storage/api/v1/file/TermsofServices.pdf"
+      : "https://staging.foodriver.site/storage/api/v1/file/TermsofServices.pdf";
 }
